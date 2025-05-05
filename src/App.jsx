ec617dfbd6a147607bf316534ec64521eabbe849
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-05-05 05:41:49 +0000

    routing for pages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -6,13 +8,14 @@ const App = () => {
       <nav>
         <h2>Shopping Cart App</h2>
         <div className="pages">
-          <button>Home</button>
-          <button>Shop</button>
+          <button><Link to="/" className='link'>Home</Link></button>
+          <button><Link to="/shop" className='link'>Shop</Link></button>
         </div>
         <div className="cart">
-          <button>Cart</button>
+          <button><Link to="/cart" className='link'>Cart</Link></button>
         </div>
       </nav>
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import Shop from './Shop.jsx';
+import Cart from './Cart.jsx';
+import ErrorPage from './ErrorPage.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/shop",
+        element: <Shop />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      }
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
