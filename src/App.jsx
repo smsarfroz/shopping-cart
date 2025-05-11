196e6f1361ca343a2603f8fcff1a7ba161c94bec
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-05-11 05:07:05 +0000

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,58 +4,59 @@ import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
 
-const App = () => {
-  const [qtyArray, setQtyArray] = useState(Array(20).fill(0));
-  const [itemArray, setItemArray] = useState([]);
-  const [orderedIds, setOrderedIds] = useState([]);
-  const [total, setTotal] = useState(0);
+const App = () => <h1>Our First Test</h1>;
+// const App = () => {
+//   const [qtyArray, setQtyArray] = useState(Array(20).fill(0));
+//   const [itemArray, setItemArray] = useState([]);
+//   const [orderedIds, setOrderedIds] = useState([]);
+//   const [total, setTotal] = useState(0);
 
-  function totalItemsInCart() {
-    let totQty = 0;
-    qtyArray.forEach((element) => {
-      totQty += element;
-    });
-    return totQty;
-  }
-  return (
-    <div>
-      <nav>
-        <b className="Title">Shopping Cart App</b>
-        <div className="pages">
-          <button className="button">
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </button>
-          <button className="button">
-            <Link to="/shop" className="link">
-              Shop
-            </Link>
-          </button>
-        </div>
-        <div className="cart">
-          <button className="cartButton">
-            <Link to="/cart" className="link">
-              <CiShoppingCart />
-              <div className="circle">{totalItemsInCart()}</div>
-            </Link>
-          </button>
-        </div>
-      </nav>
-      <Outlet
-        context={{
-          qtyArray,
-          setQtyArray,
-          itemArray,
-          setItemArray,
-          orderedIds,
-          setOrderedIds,
-          total,
-          setTotal,
-        }}
-      />
-    </div>
-  );
-};
+//   function totalItemsInCart() {
+//     let totQty = 0;
+//     qtyArray.forEach((element) => {
+//       totQty += element;
+//     });
+//     return totQty;
+//   }
+//   return (
+//     <div>
+//       <nav>
+//         <b className="Title">Shopping Cart App</b>
+//         <div className="pages">
+//           <button className="button">
+//             <Link to="/" className="link">
+//               Home
+//             </Link>
+//           </button>
+//           <button className="button">
+//             <Link to="/shop" className="link">
+//               Shop
+//             </Link>
+//           </button>
+//         </div>
+//         <div className="cart">
+//           <button className="cartButton">
+//             <Link to="/cart" className="link">
+//               <CiShoppingCart />
+//               <div className="circle">{totalItemsInCart()}</div>
+//             </Link>
+//           </button>
+//         </div>
+//       </nav>
+//       <Outlet
+//         context={{
+//           qtyArray,
+//           setQtyArray,
+//           itemArray,
+//           setItemArray,
+//           orderedIds,
+//           setOrderedIds,
+//           total,
+//           setTotal,
+//         }}
+//       />
+//     </div>
+//   );
+// };
 
 export default App;
