f7933eac5e7deae5505972a39288c336f506e8c3
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-05-11 10:11:05 +0000

    trying to test home button event

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const [qtyArray, setQtyArray] = useState(Array(20).fill(0));
@@ -18,43 +19,45 @@ const App = () => {
     return totQty;
   }
   return (
-    <div>
-      <nav>
-        <b className="Title">Shopping Cart App</b>
-        <div className="pages">
-          <button className="button">
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </button>
-          <button className="button">
-            <Link to="/shop" className="link">
-              Shop
-            </Link>
-          </button>
-        </div>
-        <div className="cart">
-          <button className="cartButton">
-            <Link to="/cart" className="link">
-              <CiShoppingCart />
-              <div className="circle">{totalItemsInCart()}</div>
-            </Link>
-          </button>
-        </div>
-      </nav>
-      <Outlet
-        context={{
-          qtyArray,
-          setQtyArray,
-          itemArray,
-          setItemArray,
-          orderedIds,
-          setOrderedIds,
-          total,
-          setTotal,
-        }}
-      />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <b className="Title">Shopping Cart App</b>
+          <div className="pages">
+            <button className="button">
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </button>
+            <button className="button">
+              <Link to="/shop" className="link">
+                Shop
+              </Link>
+            </button>
+          </div>
+          <div className="cart">
+            <button className="cartButton">
+              <Link to="/cart" className="link">
+                <CiShoppingCart />
+                <div className="circle">{totalItemsInCart()}</div>
+              </Link>
+            </button>
+          </div>
+        </nav>
+        <Outlet
+          context={{
+            qtyArray,
+            setQtyArray,
+            itemArray,
+            setItemArray,
+            orderedIds,
+            setOrderedIds,
+            total,
+            setTotal,
+          }}
+        />
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
     it('true to be true', () => {
@@ -10,5 +10,19 @@ describe('App', () => {
     it('renders App component', () => {
         const { container } = render(<App />);
         expect(container).toMatchSnapshot();
+    })
+    it('should navigaet to Home page after its button click', async () => {
+        const user = userEvent.setup();
+
+        render(<App />);
+        expect(screen.getByText(/home/i)).toBeInTheDocument();
+
+        await user.click(screen.getByText(/home/i));
+        // expect(screen.getByText(/curated./i)).toBeInTheDocument();
+        // expect(screen.getByRole("heading", { level: 1 })).textContent.toMatch(/curated./i);
+        // expect(screen.getAllByRole("heading", { level: 1 })).textContent.toMatch(/curated. effortless. yours./i);
+        // expect(screen.getByRole("heading", { level: 1, name: "curated." })).toBeInTheDocument();
+        const h1Element = screen.getByRole('heading', { name: "Curated." });
+        expect(h1Element).toBeInTheDocument();
     })
 });
