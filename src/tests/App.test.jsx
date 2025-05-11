853ce7b93a2069c7b2108c3b6521a0e1b3fc8adf
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-05-11 10:52:06 +0000

    latest commit

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -2,6 +2,8 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import userEvent from '@testing-library/user-event';
+import Home from '../Home';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App', () => {
     it('true to be true', () => {
@@ -11,18 +13,20 @@ describe('App', () => {
         const { container } = render(<App />);
         expect(container).toMatchSnapshot();
     })
-    it('should navigaet to Home page after its button click', async () => {
+    it('should navigate to Home page after its button click', async () => {
         const user = userEvent.setup();
 
         render(<App />);
         expect(screen.getByText(/home/i)).toBeInTheDocument();
 
         await user.click(screen.getByText(/home/i));
-        // expect(screen.getByText(/curated./i)).toBeInTheDocument();
+        render(<Home />, { wrapper: BrowserRouter });
+        expect(screen.getByText(/curated./i)).toBeInTheDocument();
+        expect(screen.getByText(/effortless./i)).toBeInTheDocument();
         // expect(screen.getByRole("heading", { level: 1 })).textContent.toMatch(/curated./i);
-        // expect(screen.getAllByRole("heading", { level: 1 })).textContent.toMatch(/curated. effortless. yours./i);
+        expect(screen.getAllByRole("heading", { level: 1 })).textContent.toMatch(/curated. effortless. yours./i);
         // expect(screen.getByRole("heading", { level: 1, name: "curated." })).toBeInTheDocument();
-        const h1Element = screen.getByRole('heading', { name: "Curated." });
-        expect(h1Element).toBeInTheDocument();
+        // const h1Element = screen.getByRole('heading', { name: "Curated." });
+        // expect(h1Element).toBeInTheDocument();
     })
 });
